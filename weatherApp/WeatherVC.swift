e0349b3c387250422b4160c5a7cbf364b2b952c0
Saday <ahmetsaday34@gmail.com>
2017-06-06 15:56:47 +0300

    Created weather screen and WeatherVC

---
+++ weatherApp/WeatherVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Banu on 6.06.2017.
+//  Copyright © 2017 ahmetsaday. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var lbl_date: UILabel!
+    @IBOutlet weak var lbl_currentTemp: UILabel!
+    @IBOutlet weak var lbl_location: UILabel!
+    @IBOutlet weak var img_currentWeather: UIImageView!
+    @IBOutlet weak var lbl_currentWeatherType: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
